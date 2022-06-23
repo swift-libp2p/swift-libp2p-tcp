e19f8cc6b697e5c4f8be2999929bf967c6429f81
Brandon <btoms.20@gmail.com>
2022-06-23 09:38:31 -0700

    Misc Tests

--- Tests/LibP2PTCPTests/LibP2PTCPTests.swift
+++ Tests/LibP2PTCPTests/LibP2PTCPTests.swift
@@ -6,6 +6,6 @@ final class LibP2PTCPTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LibP2PTCP().text, "Hello, World!")
+        //XCTAssertEqual(LibP2PTCP().text, "Hello, World!")
     }
 }
